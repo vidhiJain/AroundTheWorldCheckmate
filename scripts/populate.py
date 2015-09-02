b4af8d44a9f89620ecac49158aee18335bbfd0b3
Eklavya Sharma <sharmaeklavya2@hotmail.com>
2015-09-03 02:58:09 +0530

    Improved population script
    Implemented distance calculation
    Old objects are now deleted before adding new ones

--- scripts/populate.py
+++ scripts/populate.py
@@ -7,12 +7,26 @@ if BASE_DIR not in sys.path:
 DEFAULT_QUESTIONS_FILE_PATH = os.path.join(BASE_DIR, "data", "questions.json")
 
 import json
+import math
 
-def get_dist(lat1,long1,lat2,long2):
-	return 0
+def square(n):
+	return n*n
 
-def add_data_from_file(file_path):
+def get_dist(phi1,phi2,dlmd):
+	sinp1 = math.sin(phi1*math.pi/180.0)
+	sinp2 = math.sin(phi2*math.pi/180.0)
+	sindl = math.sin(dlmd*math.pi/180.0)
+	cosp1 = math.cos(phi1*math.pi/180.0)
+	cosp2 = math.cos(phi2*math.pi/180.0)
+	cosdl = math.cos(dlmd*math.pi/180.0)
+	num = math.sqrt(square(cosp2*sindl)+square(cosp1*sinp2-sinp1*cosp2*cosdl))
+	den = sinp1*sinp2+cosp1*cosp2*cosdl
+	return settings.CONFIG["earth_radius_in_km"] * math.atan2(num,den)
+
+def set_data_from_file(file_path):
 	ques_dict = json.load(open(file_path))
+	print("Loading locations...")
+	Question.objects.all().delete()
 	for loc_name, ques in ques_dict.items():
 		db_ques = Question(loc_name=loc_name)
 		fields = ('answer','rent','stipend')
@@ -21,11 +35,16 @@ def add_data_from_file(file_path):
 			if field in ques:
 				setattr(db_ques,field,ques[field])
 		db_ques.save()
+	print("Precomputing distances...")
+	Distance.objects.all().delete()
 	for source_name, source_data in ques_dict.items():
 		if "latitude" in source_data and "longitude" in source_data:
 			for dest_name, dest_data in ques_dict.items():
 				if "latitude" in dest_data and "longitude" in dest_data:
-					dist = get_dist(source_data["latitude"], source_data["longitude"], dest_data["latitude"], dest_data["longitude"])
+					if dest_name==source_name:
+						dist=0
+					else:
+						dist = get_dist(source_data["latitude"], dest_data["latitude"], abs(source_data["longitude"] - dest_data["longitude"]))
 					source_db_obj = Question.objects.get(loc_name=source_name)
 					dest_db_obj = Question.objects.get(loc_name=dest_name)
 					Distance(source=source_db_obj, dest=dest_db_obj, distance=dist).save()
@@ -37,6 +56,7 @@ if __name__=="__main__":
 	django.setup()
 
 from main.models import Question, Distance
+from django.conf import settings
 
 if __name__=="__main__":
-	add_data_from_file(DEFAULT_QUESTIONS_FILE_PATH)
+	set_data_from_file(DEFAULT_QUESTIONS_FILE_PATH)
